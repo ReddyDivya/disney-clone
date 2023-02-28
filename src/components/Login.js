7ef74eab4cbfcb63f07374474c4eab472a812406
Divya <divyareddy0205@gmail.com>
2023-02-28 15:45:01 -0800

    updating logo src

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,6 +10,7 @@ const Login = (props) => {
                     <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
                     <SignUp>GET ALL THERE</SignUp>
                     <Description>Get Premier Access to Rayna and the Last Dragon for an additional fee with a Disney+ subscription. As of 03/26/2023, the price of Disney+ and The Disney Bundle will increase by $1.</Description>
+                    <CTALogoTwo src="/images/cta-logo-two.png" alt="" />
                 </CTA>
                 <BgImage />
             </Content>
@@ -103,6 +104,14 @@ font-size:11px;
 margin:0 0 24px;
 line-height:1.5;
 letter-spacing:1.3px;
+`
+
+const CTALogoTwo = styled.img`
+max-width:600px;
+margin-bottom:20px;
+display:inline-block;
+vertical-align:bottom;
+width:100%;
 `
 
 export default Login
