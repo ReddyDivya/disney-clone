61a6c30c3da1d2bb462f741273f83ac9fde22a07
Divya <divyareddy0205@gmail.com>
2023-02-28 18:14:33 -0800

    adding nav-menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,14 @@ import styled from "styled-components";
 
 const Header = (props) => {
     return (
-        <Nav><Logo><img src="/images/logo.svg" alt="Disney+" /></Logo></Nav>
+        <Nav>
+            <Logo><img src="/images/logo.svg" alt="Disney+" /></Logo>
+            <NavMenu>
+                <a href="/home"><img src="/images/home-icon.svg" alt="Home" /></a>
+                <span>HOME</span>
+            </NavMenu>
+
+        </Nav>
     )
 }
 
@@ -35,5 +42,70 @@ img{
     width:100%;
 }
 `;
+
+const NavMenu = styled.div`
+align-items:center;
+display:flex;
+flex-flow:row nowrap;
+height:100%;
+justify-content:flex-end;
+margin:0px;
+padding:0px;
+position:relative;
+margin-right:auto;
+margin-left:25px;
+
+a{
+    display:flex;
+    align-items:center;
+    padding:0 12px;
+
+    img{
+        height:20px;
+        min-width:20px;
+        width:20px;
+        z-index:auto;
+    }
+
+    span{
+        color: rgb(249, 249, 249);
+        font-size:13px;
+        letter-spacing:1.42px;
+        line-height:1.08;
+        padding:2px 0px;
+        white-space:nowrap;
+        position:relative;  
+    
+        &:before{
+            background-color:rgb(249, 249, 249);
+            border-radius:0px 0px 4px 4px;
+            bottom:-6px;
+            left:0px;
+            content:"";
+            height:2px;
+            opacity:0;
+            position:absolute;
+            right:0px;
+            transform-origin: left center;
+            transform:scaleX(0) ;
+            transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+            visibility:hidden;
+            width:auto;
+        }
+    }
+
+    &:hover{
+        span:before{
+            transform:scaleX(1);
+            visibility:visible;
+            opacity:1 !important;
+        }
+    }
+}
+
+@media (max-width: 768px){
+    display:none;
+}
+`
 
 export default Header
