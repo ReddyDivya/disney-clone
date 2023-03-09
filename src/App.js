656f1cbc715ee7575ed9290268829d3582b9cb60
Divya <divyareddy0205@gmail.com>
2023-03-08 17:24:16 -0800

    working on home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from "./components/Login";
 import Header from "./components/Header";
+import Home from "./components/Home";
 import './App.css';
 import React from 'react';
 
@@ -11,6 +12,7 @@ function App() {
         <Header />
         <Routes>
           <Route exact path="/" element={<Login />} />
+          <Route exact path="/home" element={<Home />} />
         </Routes>
       </Router>
     </div>
---
+++ src/components/Home.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Home = (props) => {
+
+}
+
+
+export default Home;
