bcc4c35f026376e355e3b78643c8d1ace807c0a3
Divya <divyareddy0205@gmail.com>
2023-03-08 20:21:18 -0800

    added react-slick

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -1,11 +1,32 @@
 import styled from 'styled-components';
+import React from 'react';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Slider from "react-slick";
 
-import React from 'react'
+const ImgSlider = (props) => {
+    let settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+    }
 
-function ImgSlider() {
-    return (
-        <div>ImgSlider</div>
-    )
+    return (<div>
+        <Slider {...settings}>
+            <div>
+                <h3>1</h3>
+            </div>
+            <div>
+                <h3>2</h3>
+            </div>
+            <div>
+                <h3>3</h3>
+            </div>
+        </Slider>
+    </div>);
 }
 
 export default ImgSlider;
