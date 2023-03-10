878b306ee4d407b65099ca99903a8934b9902f1e
Divya <divyareddy0205@gmail.com>
2023-03-09 20:34:00 -0800

    started working on recommended components

---
+++ src/components/Recommends.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from "styled-components";
+
+const Recommends = (props) => {
+    return (
+        <h4>Recommended for You</h4>
+    )
+}
+
+export default Recommends;
