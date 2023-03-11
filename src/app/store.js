8e35b21f08eacd43f229a7b3fbe7f1d76aee3800
Divya <divyareddy0205@gmail.com>
2023-03-10 20:14:22 -0800

    fetching data from firebase db

--- src/app/store.js
+++ src/app/store.js
@@ -3,11 +3,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import logger from 'redux-logger';
 // import rootReducer from './reducer';
 import userReducer from "../features/user/userSlice";
+import movieReducer from "../features/movie/movieSlice";
 
 // Store has all of the default middleware added, _plus_ the logger middleware
 export default configureStore({
     reducer: {
         user: userReducer,
+        movie: movieReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 })
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,60 @@ import Recommends from "./Recommends";
 import NewDisney from "./NewDisney";
 import Originals from "./Originals";
 import Trending from "./Trending";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import db from "../firebase"; //using database from firebase
+import { setMovies } from "../features/movie/movieSlice";
+import { selectUserName } from "../features/user/userSlice";
 
 const Home = (props) => {
+
+    const dispatch = useDispatch();
+    const userName = useSelector(selectUserName);
+    let recommends = [];
+    let newDisneys = [];
+    let originals = [];
+    let trending = [];
+
+    useEffect(() => {
+
+        //movies is a collection name of the firebase db
+        db.collection("movies").onSnapshot((snapshot) => {
+            //docs is a list of records of the firebase db
+            snapshot.docs.map((doc) => {
+                //doc is an individual records of the firebase db
+                //type is field name
+                //...doc.data() - this means entire data
+                switch (doc.data().type) {
+                    case "recommend":
+                        recommends = [...recommends, { id: doc.id, ...doc.data() }]; //...recommends - keep the existing, {object} - add new record to it
+                        break;
+
+                    case "new":
+                        newDisneys = [...newDisneys, { id: doc.id, ...doc.data() }];
+                        break;
+
+                    case "original":
+                        originals = [...originals, { id: doc.id, ...doc.data() }];
+                        break;
+
+                    case "trending":
+                        trending = [...trending, { id: doc.id, ...doc.data() }];
+                        break;
+                }
+            });
+
+            dispatch(
+                setMovies({
+                    recommend: recommends,
+                    newDisney: newDisneys,
+                    original: originals,
+                    trending: trending,
+                })
+            );
+        });
+    }, [userName]); //update it when username gets updated
+
     return <Container>
         <ImgSlider />
         <Viewers />
